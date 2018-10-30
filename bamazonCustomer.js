213e09dec4a3d2f59f829abab9af2025778e09cd
dulcineapena1 <f.dulcinea.pena@gmail.com>
2018-10-29 20:47:23 -0600

    actualización 1

---
+++ bamazonCustomer.js
@@ -0,0 +1,101 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
+
+//------INICIO PARTE MOSTRAR PRODUCTOS
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+   
+    //esto muestra todo lo que está en la tabla products
+    connection.query("SELECT * FROM products", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+    busqueda();
+});
+
+
+//estas variables están aquí para acutualizarlas en diferentes functions y usarlas en diferentes fuctions también
+var elid=null;
+var stockactual=null;
+var costo=null;
+
+
+//------INICIO PARTE BUSCAR EL ID DEL PRODUCTO
+function busqueda() {
+    inquirer
+    .prompt({
+        name: "item_id",
+        type: "input",
+        message: "Introduce el ID del producto"
+      })
+      .then(function(user) {
+        //le pongo a la variable el valor que puso en input
+        elid=user.item_id; 
+                            //aquí le pongo las cosas a sacar de la base de datos
+        var query = "SELECT item_id,product_name,department_name,price,stock_quiantity FROM products WHERE ?";
+                                    //aquí le digo que el item_id de la base de datos va a ser el que puso en este input
+        connection.query(query, { item_id: user.item_id }, function(err, res) {
+            console.log("ITEM: " + res[0].item_id + " || PRODUCT: " + res[0].product_name + " || DEPARTMENT: " + res[0].department_name + " || PRICE: " + res[0].price + " || STOCK: " + res[0].stock_quiantity );
+            //aquí pongo en esa variable el valor que ya saqué aquí en el input
+            stockactual=res[0].stock_quiantity;
+            costo= res[0].price;
+        // for (var i = 0; i < res.length; i++) {
+        //  console.log("Position: " + res[i].item_id + " || Song: " + res[i].product_name );
+        //  }
+        unidades();
+        });
+      });
+}
+  
+//-------INICIO PARTE COMPRA
+  function unidades() {
+    inquirer
+    .prompt({
+        name: "quitar",
+        type: "input",
+        message: "Número de unidades a comprar:"
+      })
+      .then(function(user) {
+          //aquí borro lo que está en esa variable menos el valor que puso en input
+          var borrar= stockactual-user.quitar;
+          //aquí muestro como costo final el costo del producto * lo que introdujo el input
+          var costofinal= costo * user.quitar;
+
+            //si es más lo que quiere comprar de lo que hay
+            if(borrar<=0){
+                console.log("No hay suficiente stock! Elige otro producto");
+                busqueda();
+            }
+            //si sí hay stock suficiente
+            else{          
+                var query = "UPDATE products SET ? WHERE ?";
+                                            //aquí pongo que el stock_quiantity va a setearse con lo que tenga la variable borrar, del item que tenga el valor de la variable elid
+                connection.query(query,[ {stock_quiantity: borrar},{item_id: elid}], function(err, res) {
+                   // console.log(res.affectedRows + " Producto agregado!\n");
+                    console.log("Producto(s) agregado(s), Total a pagar:"+ costofinal);
+                    
+                    //cierra el puerto SIEMPRE HAY QUE PONERLO AL FINAL DE LA APLICACIÓN
+                    connection.end();
+                });
+            }//fin else       
+      });
+  }
+  
+
+
+
---
+++ bamazonManager.js
@@ -0,0 +1,183 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  menu();
+});
+
+
+
+
+
+function menu() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "¿Qué quieres hacer?",
+      choices: [
+        "Ver productos a la venta",
+        "Ver bajo inventario",
+        "Agregar a inventario",
+        "Agregar nuevo producto"
+      ]
+    })
+    .then(function(usuario) {
+      switch (usuario.action) {
+      case "Ver productos a la venta":
+        vertodo();
+        break;
+
+      case "Ver bajo inventario":
+        bajoinvenatario();
+        break;
+
+      case "Agregar a inventario":
+        agregarinventario();
+        break;
+
+      case "Agregar nuevo producto":
+        agregarnuevoproducto();
+        break;
+      }
+    });
+}
+
+function vertodo(){   
+          //esto muestra todo lo que está en la tabla products
+          connection.query("SELECT * FROM products", function (err, result) {
+              if (err) throw err;
+              console.log(result);
+          });
+          menu();
+         
+}
+
+
+
+
+function bajoinvenatario(){
+                                                                                                        //DONDE inventario sea igual o menor a 5
+    var query = "SELECT item_id,product_name,department_name,price,stock_quiantity FROM products WHERE stock_quiantity <= 5 ";
+    connection.query(query, function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log("ITEM: " + res[i].item_id + " || PRODUCT: " + res[i].product_name + " || DEPARTMENT: " + res[i].department_name + " || PRICE: " + res[i].price + " || STOCK: " + res[i].stock_quiantity );
+      }
+      menu();
+    });
+}
+
+
+
+//estas variables son para obtener los valores que se utilizarán en la function agregarinventario
+var elid=null;
+var stockactual=0; 
+
+
+function agregarinventario(){
+    inquirer
+    .prompt([{
+        name: "idparainventario",
+        type: "input",
+        message: "Escríbe el ID al cuál deseas agregar stock:"
+      },
+      {
+        name: "agregarstock",
+        type: "input",
+        message: "Unidades a agregar:"
+      }
+    
+    ])
+      .then(function(user) {   
+            //--INICIO LEER EL STOCK
+                //le pongo a la variable el valor que puso en input
+                elid=user.idparainventario; 
+                                        //aquí le pongo las cosas a sacar de la base de datos
+                var query = "SELECT item_id,product_name,department_name,price,stock_quiantity FROM products WHERE ?";
+                                             //aquí le digo que el item_id de la base de datos va a ser el que puso en este input
+                connection.query(query, { item_id: user.idparainventario}, function(err, res) {
+                    //console.log("ITEM: " + res[0].item_id + " || PRODUCT: " + res[0].product_name + " || DEPARTMENT: " + res[0].department_name + " || PRICE: " + res[0].price + " || STOCK: " + res[0].stock_quiantity );
+                    //aquí pongo en esa variable el valor que ya saqué aquí en el input
+                    stockactual=res[0].stock_quiantity;
+                    var paraagregar= stockactual + parseInt(user.agregarstock);
+                    //esta función que va a correr es importante que esté aquí pues si no se saldría del connection query y lo que esté aquí adentro como valor, se perdería para próximos usos
+                    agregando();
+
+                     //--INICIO ACTUALIZAR STOCK
+                function agregando(){   
+                    var query = "UPDATE products SET ? WHERE ?";
+                    //aquí pongo que el stock_quiantity va a setearse con lo que tenga la variable borrar, del item que tenga el valor de la variable elid
+                    connection.query(query,[ {stock_quiantity: paraagregar},{item_id: elid}], function(err, res) {
+                        // console.log(res.affectedRows + " Producto agregado!\n");
+                        console.log("Se agregó al ID:"+ user.idparainventario + "el stock de:" + user.agregarstock);
+
+                        menu();
+                    });
+                }//--FIN ACTUALIZAR STOCK   
+
+                }); //---FIN LEER EL STOCK               
+      });
+}
+
+
+
+function agregarnuevoproducto(){
+    inquirer
+    .prompt([{
+        name: "producto",
+        type: "input",
+        message: "Nombre del producto:"
+      },
+      {
+        name: "departamento",
+        type: "input",
+        message: "Nombre del departamento:"
+      },
+      {
+        name: "precio",
+        type: "input",
+        message: "Precio:"
+      },
+      {
+        name: "stockinicial",
+        type: "input",
+        message: "Stock inicial:"
+      }
+    
+    ])
+      .then(function(user) {
+
+            console.log("Creando nuevo producto...\n");
+            var query = connection.query(
+                "INSERT INTO products SET ?",
+                {
+                    product_name: user.producto,
+                    department_name: user.departamento,
+                    price: user.precio,
+                    stock_quiantity: user.stockinicial
+                },
+                function(err, res) {
+                    console.log(res.affectedRows + " Productos insertados exitósamente \n");
+                    menu();
+                }
+            );
+
+
+   
+    });
+}
